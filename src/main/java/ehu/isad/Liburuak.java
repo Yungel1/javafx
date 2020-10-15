658c5910900067d7fdcc3de6054a3102a99248fc
Adrián Sáanchez <adriansanchez0123@gmail.com>
2020-10-15 14:11:16 +0200

    Egitura eginda

---
+++ src/main/java/ehu/isad/Liburuak.java
@@ -0,0 +1,16 @@
+package ehu.isad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Liburuak extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
